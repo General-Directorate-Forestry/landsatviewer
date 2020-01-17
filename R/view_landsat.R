c2a3e4f3aa4a89d435a45d6ebb82a5b7dce74c68
Henry Rodman <henry.rodman@gmail.com>
2020-01-17 08:17:40 -0600

    throw error if no path/row/date or scene is provided

--- R/view_landsat.R
+++ R/view_landsat.R
@@ -16,6 +16,10 @@
 view_landsat <- function(p = NULL, r = NULL, date = NULL,
                          scene = NULL, open_rasts = FALSE) {
 
+  if (is.null(scene) | (is.null(p) & is.null(r) & is.null(date))) {
+    stop("you must specifiy either a row from get_scene_table() or a path/row/date combo")
+  }
+
   if (is.null(scene)) {
     all_scenes <- get_scene_table()
 
